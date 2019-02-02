f19e4a1727bbd28b3454b09c5fec9d9cafec595e
Ugur Gudelek <ugurgudelek@gmail.com>
2019-02-03 00:23:52 +0300

    log functionality added for plot.py

--- spark-lr/plot.py
+++ spark-lr/plot.py
@@ -1,5 +1,4 @@
 import argparse
-
 import matplotlib.pyplot as plt
 from mpl_toolkits import mplot3d
 import numpy as np
@@ -8,6 +7,8 @@ import pandas as pd
 
 parser = argparse.ArgumentParser()
 parser.add_argument("path", help="Path for result file to parse and plot")
+parser.add_argument("--log_datasize", action='store_true', default=False, help="Take a log for datasize values")
+parser.add_argument("--log_elapsedtime", action='store_true', default=False, help="Take a log for elapsedtime values")
 args = parser.parse_args()
 
 result_df = pd.read_csv(args.path, sep=' - ', header=None, engine='python')
@@ -23,6 +24,13 @@ cores = result_df['core'].unique()
 datasize = result_df['size'].unique()
 elapsed_time = result_df['elapsed_time'].values.reshape(set(datasize).__len__(),-1)
 
+# log_datasize
+datasize = np.log(datasize) if args.log_datasize else datasize
+ylabel = 'log(datasize)' if args.log_datasize else 'datasize'
+
+# log_elapsedtime
+elapsed_time = np.log(elapsed_time) if args.log_elapsedtime else elapsed_time
+zlabel = 'log(elapsed time(sec))' if args.log_elapsedtime else 'elapsed time (sec)'
 
 def plot_3dsurface(x,y,z, params):
     fig = plt.figure()
@@ -40,6 +48,6 @@ def plot_3dsurface(x,y,z, params):
 
 plot_3dsurface(x=cores, y=datasize, z=elapsed_time,
                params={'xlabel':'# core',
-                       'ylabel':'datasize',
-                       'zlabel':'elapsed time (sec)',
+                       'ylabel':ylabel,
+                       'zlabel':zlabel,
                        'title':'spark benchmark (datasize vs #cores)'}).show()
